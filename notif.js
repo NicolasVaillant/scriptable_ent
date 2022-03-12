1e1cabacac1329befc5ad91936d3371efc95c5b8
Nicolas <77850354+NicolasVaillant@users.noreply.github.com>
2022-03-12 15:55:22 +0100

    Update notif.js
    Avoid error for undefined passed args
    Notification style changed

--- notif.js
+++ notif.js
@@ -1,12 +1,26 @@
-
 const n = new Notification()
 
+// Passing Arguments here
 let textVar = args.plainTexts.toString()
 
-n.title = ""
-n.subtitle = textVar.split(",")[0]
-n.body = textVar.split(",")[1]
+n.title = "Emploi du temps 📚"
+n.subtitle = ""
 
-n.threadIdentifier = "Notifications"
+// Avoid error for passed args
+if(textVar === null || textVar === undefined || textVar === ""){       
+        n.body = "Aucun résultat"
+}else{ 
+        n.body = textVar.split(",")[1]
+}
+
+n.threadIdentifier = "Emploi du temps"
+
+ENT_url = "https://ent.istp-france.com/ENT/Eleve/MonPlanning.aspx";
+
+n.openURL = ENT_url
+
+// Add action when the notification is held : haptic touch (Apple)
+n.addAction("Ouvrir l'ENT", ENT_url, false)
+
 n.schedule()
 Script.complete()
